b55f1fad175ef3d5f33a80ca1387bede158e567a
Zordlan <fiercefalcon14@gmail.com>
2021-07-25 07:15:50 -0700

    errors

--- src/listeners/errors/commandError.ts
+++ src/listeners/errors/commandError.ts
@@ -1,15 +1,15 @@
-// import { BotListener } from '@extensions/BotListener';
-// import utils from '@functions/utils'
+import { BotListener } from '@extensions/BotListener';
+import utils from '@functions/utils'
 
-// export default class commandErrorListener extends BotListener {
-//     constructor() {
-//         super('commandErrorListener', {
-//             emitter: 'commandHandler',
-//             event: 'error'
-//         })
-//     }
-//     async exec(error, message) {
-//         if (this.client.ownerID.includes(message.author.id)) { message.reply(`An error occured!\n\`\`\`js\n${error.stack}\`\`\``) }
-//         else { message.reply({ embeds: [this.client.error(error, ' command')] }) }
-//     }
-// }
+export default class commandErrorListener extends BotListener {
+    constructor() {
+        super('commandErrorListener', {
+            emitter: 'commandHandler',
+            event: 'error'
+        })
+    }
+    async exec(error, message) {
+        if (this.client.ownerID.includes(message.author.id)) { message.reply(`An error occured!\n\`\`\`js\n${error.stack}\`\`\``) }
+        else { message.reply({ embeds: [this.client.error(error, ' command')] }) }
+    }
+}
--- src/listeners/errors/miscError.ts
+++ src/listeners/errors/miscError.ts
@@ -1,23 +1,23 @@
-// import { BotListener } from '@extensions/BotListener';
-// import utils from '@functions/utils'
+import { BotListener } from '@extensions/BotListener';
+import utils from '@functions/utils'
 
-// export default class miscErrorListener extends BotListener {
-//     constructor() {
-//         super('miscErrorListener', {
-//             emitter: 'process',
-//             event: 'unhandledRejection'
-//         })
-//     }
-//     async exec(error) {
-//         if (error == "TypeError: Cannot read property 'send' of undefined" && this.client.user == null) {
-//             console.log(`Invalid token, or other login error.`)
-//             process.exit()
-//         }
+export default class miscErrorListener extends BotListener {
+    constructor() {
+        super('miscErrorListener', {
+            emitter: 'process',
+            event: 'unhandledRejection'
+        })
+    }
+    async exec(error) {
+        if (error == "TypeError: Cannot read property 'send' of undefined" && this.client.user == null) {
+            console.log(`Invalid token, or other login error.`)
+            process.exit()
+        }
 
-//         if (error == "TypeError: Cannot read property 'send' of undefined" && this.client.user != null) {
-//             console.error(error)
-//         }
+        if (error == "TypeError: Cannot read property 'send' of undefined" && this.client.user != null) {
+            console.error(error)
+        }
 
-//         this.client.error(error)
-//     }
-// }
+        this.client.error(error)
+    }
+}
