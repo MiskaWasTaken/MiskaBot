d3e2253e6803c2cda6f0c44f0f654a3430b39ad9
Zordlan <fiercefalcon14@gmail.com>
2021-07-25 12:28:27 -0700

    fuck you, india government

--- src/listeners/errors/commandError.ts
+++ src/listeners/errors/commandError.ts
@@ -9,6 +9,8 @@ export default class commandErrorListener extends BotListener {
         })
     }
     async exec(error, message) {
+        if (error == 'Error: Request failed with status code 451') { return message.reply('For whatever stupid reason, the government blocked me from accessing this when I\'m hosted in this location.') }
+
         if (this.client.ownerID.includes(message.author.id)) { message.reply(`An error occured!\n\`\`\`js\n${error.stack}\`\`\``) }
         else { message.reply({ embeds: [this.client.error(error, ' command')] }) }
     }
