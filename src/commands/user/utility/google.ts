061a106d4b2ea1d7e6e89b260493eff9a7475044
MiskaWasTaken <aaryamungli@gmail.com>
2021-07-26 02:46:55 +0530

    google command

---
+++ src/commands/user/utility/google.ts
@@ -0,0 +1,51 @@
+import { BotCommand } from '@extensions/BotCommand';
+import utils from '@functions/utils';
+import { MessageEmbed } from 'discord.js';
+const googleIt = require('google-it')
+
+export default class templateCommand extends BotCommand {
+    constructor() {
+        super('google', {
+            aliases: ['google'],
+            description: 'When your browser breaks moment',
+            usage: '$google <query>',
+            args: [
+                {
+                    id: 'google',
+                    type: 'string',
+                    match: 'restContent'
+                }
+            ]
+        })
+    }
+    async exec(message, args) {
+
+        if (!message.channel.nsfw) { return message.reply({ embeds: [this.client.notNsfwEmbed] }) }
+
+        const embed = new MessageEmbed()
+            .setTitle("Google Search Results")
+            .setColor('RANDOM')
+            .setTimestamp()
+                
+            let google = args.google
+
+            const noarg = new MessageEmbed()
+            .setTitle(`Google Command`)
+            .setDescription(`You need to provide a query.\n example: $google discord`)
+            .setColor('RANDOM')
+
+            if (!args.google) {return message.reply({ embeds: [noarg] })};
+
+        googleIt({'query': google[0]}).then(results => {
+            results.forEach(function(item, index) { 
+                embed.addField((index + 1) + ": " + item.title, "<" + item.link + ">");
+            });
+            
+            message.reply({ embeds: [embed] })
+        })
+            // any possible errors that might have occurred (like no Internet connection)
+        };
+    };
+    
+
+    
