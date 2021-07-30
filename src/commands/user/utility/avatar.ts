c5166d5ec4b71c162f25dbbfd880e7120faa9b0f
Zordlan <fiercefalcon14@gmail.com>
2021-07-29 21:39:57 -0700

    av command

--- src/commands/user/utility/avatar.ts
+++ src/commands/user/utility/avatar.ts
@@ -1,31 +1,32 @@
-import { MessageEmbed } from 'discord.js';
-import { BotCommand } from '@extensions/BotCommand';
+import { MessageEmbed } from "discord.js";
+import { BotCommand } from "@extensions/BotCommand";
 
 export default class avatar extends BotCommand {
-    constructor() {
-        super('avatar', {
-            aliases: ['avatar'],
-            description: 'Show someones avatar',
-            usage: '$avatar',
+	constructor() {
+		super("avatar", {
+			aliases: ["avatar"],
+			description: "Show someones avatar",
+			usage: "$avatar",
 
-            args: [
-                {
-                    id: 'user',
-                    type: 'user',
-                    match: 'restContent'
-                }
-            ]
-        })
-    }
+			args: [
+				{
+					id: "user",
+					type: "user",
+					match: "restContent",
+					default: (message) => message.author,
+				},
+			],
+		});
+	}
 
-    async exec(message, args) {
-        const avatar = args.user.displayAvatarURL({ size: 4096, dynamic: true })
+	async exec(message, args) {
+		const avatar = args.user.displayAvatarURL({ size: 4096, dynamic: true });
 
-        const embed = new MessageEmbed()
+		const embed = new MessageEmbed()
             .setTitle(`${args.user.tag}'s Avatar`)
             .setURL(avatar)
             .setImage(avatar)
-            .setColor('RANDOM')
-        await message.reply({ embeds: [embed] });
-    }
+            .setColor("RANDOM");
+		await message.reply({ embeds: [embed] });
+	}
 }
