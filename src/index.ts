efaf42883d08403898e5410ff2cfee0e5693ea59
MiskaWasTaken <hahano@lordgaben.steam>
2021-08-02 11:20:12 +0530

    e

--- src/index.ts
+++ src/index.ts
@@ -24,8 +24,7 @@ const distube = new DisTube.default(client, {
 	leaveOnFinish: false,
 	leaveOnStop: false,
 	plugins: [new SoundCloudPlugin(), new SpotifyPlugin()],
-})
-
+});
 
 client.on('messageCreate', (message) => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return
@@ -35,7 +34,7 @@ client.on('messageCreate', (message) => {
 
 	if (command === 'stop') {
 		distube.stop(message)
-		message.channel.send('Music stopped.')
+		message.reply('Music stopped.')
 	}
 
 	if (command === 'resume') {
@@ -80,17 +79,56 @@ client.on('messageCreate', (message) => {
 		}
 	}
 
-})
+	
+	if (
+		[
+			`3d`,
+			`bassboost`,
+			`echo`,
+			`karaoke`,
+			`nightcore`,
+			`vaporwave`,
+		].includes(command)
+	) {
+		const filter = distube.setFilter(message, command)
+		message.reply(
+			`Current queue filter: ${filter.join(', ') || 'Off'}`,
+		)
+	}
 
+	if (['repeat', 'loop'].includes(command)) {
+		const mode = distube.setRepeatMode(message)
+		message.reply(`Set repeat mode to \`${mode ? mode === 2 ? 'All Queue' : 'This Song' : 'Off'}\``)
+	}
+
+	if (['disconnect', 'leave'].includes(command)) {
+		distube.disconnect(message)
+	}
+
+})
 
 
 const status = (queue) => `Volume: \`${queue.volume}%\` | Loop: \`${queue.repeatMode ? queue.repeatMode == 2 ? "Server Queue" : "This Song" : "Off"}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``;
 
 distube.on("playSong", (queue, song) => queue.textChannel.send(
-    `Playing: \n\`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}\n${status(queue)}`
+    `Playing: \n\`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user.tag}\n${status(queue)}`
 ));
 
+distube.on('addSong', (queue, song) =>
+queue.textChannel.send(
+	`Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user.tag}`,
+))
 
+distube
+.on('searchNoResult', message => message.channel.send(`No results were found for you music.`))
+.on('finish', (queue) => queue.textChannel.send('Queue has been finished.'))
+.on('finishSong', (queue, song) => queue.textChannel.send(`Song has finished playing.`))
+.on('disconnect', (queue, song) => queue.textChannel.send('Disconnected.'))
+.on('empty', (queue, song) => queue.textChannel.send('Voice channel is empty.'))
+
+distube.on("error", (channel, error) => channel.send(
+    "An error was encountered: " + error
+));
 
 
 //console.log(process.env)
