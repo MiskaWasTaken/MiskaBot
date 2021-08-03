82b5e0f58605f3e2eac19d759bb36a45e4adcbdb
MiskaWasTaken <hahano@lordgaben.steam>
2021-08-03 21:13:18 +0530

    hostin now

--- src/listeners/errors/commandError.ts
+++ src/listeners/errors/commandError.ts
@@ -11,6 +11,10 @@ export default class commandErrorListener extends BotListener {
         //if (error == 'Error: Request failed with status code 451') { return message.reply('For whatever stupid reason, the government blocked me from accessing this when I\'m hosted in this location.') }
 
         if (this.client.ownerID.includes(message.author.id)) { message.reply(`An error occured!\n\`\`\`js\n${error.stack}\`\`\``) }
+        if(message.author.id){
+            message.reply({ embeds: [this.client.error(error, ' command')] })
+            message.reply("Something went wrong, please make sure you use the command with with correct syntax, usage and limitations. Error has been automatically reported to my devs.")
+        }
         else { message.reply({ embeds: [this.client.error(error, ' command')] }) }
     }
 }
