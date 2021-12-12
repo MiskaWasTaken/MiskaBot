946789d5a6739708a4a296d20bdfa3dfe321cad9
Miska <50771741+MiskaWasTaken@users.noreply.github.com>
2021-12-12 09:16:39 +0530

    ready for public

---
+++ src/extensions/config/config.ts
@@ -0,0 +1,16 @@
+export const token = ''
+export const google = '' (google and ytapikey are the same thing, google how to get a yt api key)
+export const ytapikey = '' 
+export const client_id = '' (follow these steps: https://github.com/googlesamples/assistant-sdk-nodejs)
+export const client_secret= ''
+export const refresh_token = ''
+export const hypixel_key = '' (get on mc.hypixel.net and do /api new get the string of numbers and letters and put that here
+
+export default {
+    token,
+    ytapikey,
+    client_id,
+    client_secret,
+    refresh_token,
+    hypixel_key
+}
