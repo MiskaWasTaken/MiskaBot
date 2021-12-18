c0f526f15d420e124ee3a84711333f5b9543b73d
RaZe <75721601+RaZeSloth@users.noreply.github.com>
2021-12-18 16:20:26 +0200

    Fixes in code

--- src/extensions/config/config.ts
+++ src/extensions/config/config.ts
@@ -1,10 +1,10 @@
 export const token = ''
-export const google = '' (google and ytapikey are the same thing, google how to get a yt api key)
+export const google = '' //(google and ytapikey are the same thing, google how to get a yt api key)
 export const ytapikey = '' 
-export const client_id = '' (follow these steps: https://github.com/googlesamples/assistant-sdk-nodejs)
+export const client_id = '' //(follow these steps: https://github.com/googlesamples/assistant-sdk-nodejs)
 export const client_secret= ''
 export const refresh_token = ''
-export const hypixel_key = '' (get on mc.hypixel.net and do /api new get the string of numbers and letters and put that here
+export const hypixel_key = '' //(get on mc.hypixel.net and do /api new get the string of numbers and letters and put that here
 
 export default {
     token,
